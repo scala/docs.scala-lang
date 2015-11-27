d0d18a502ab7a5c4d8ba70bdaf508fa56b90e83c
Rémy-Christophe Schermesser <git@schermesser.com>
2015-11-27 14:54:53 +0100

    Better UI for the search bar

--- resources/javascript/searchbar.js
+++ resources/javascript/searchbar.js
@@ -1,9 +1,9 @@
-(function() {
+(function($) {
     var template =
         '<div class="aa-dataset-a">' +
         '</div>' +
         '<div class="branding-footer">' +
-        '<div class="branding"><span>Powered by</span><a target="_blank" href="https://algolia.com"><img src="https://www.algolia.com/assets/algolia128x40.png"/></a></div>' +
+        '<div class="branding"><span>Powered by</span><a target="_blank" href="https://www.algolia.com/?utm_source=scaladocs"><img src="https://www.algolia.com/assets/algolia128x40.png"/></a></div>' +
         '</div>';
 
     var client = algoliasearch('BH4D9OD16A', '3403ae7bb4cb839fba71e2fae9ab1534');
@@ -14,6 +14,8 @@
         return string.charAt(0).toUpperCase() + string.slice(1);
     };
 
+    var hitsSource = autocomplete.sources.hits(index, { hitsPerPage: 7, tagFilters: [ 'en' ], numericFilters: 'importance>0' });
+
     autocomplete('#q', {
         hint: false,
         debug: false,
@@ -22,20 +24,58 @@
         }
     }, [
         {
-            source: autocomplete.sources.hits(index, { hitsPerPage: 5, tagFilters: [ 'en' ], numericFilters: 'importance>0' }),
+            source: function(query, callback) {
+                hitsSource(query, function(suggestions) {
+                    var categories = {};
+                    suggestions.forEach(function(suggestion) {
+                        var enIndex = suggestion._tags.indexOf('en');
+                        if(enIndex > -1) {
+                            suggestion._tags.splice(enIndex, 1);
+                        }
+                        categories[suggestion._tags[0]] = categories[suggestion._tags[0]] || []
+                        categories[suggestion._tags[0]].push(suggestion)
+                    });
+
+                    categories = $.map(Object.keys(categories).sort(), function(categoryName) {
+                        var items = categories[categoryName];
+                        items[0].isCategoryHeader = true;
+                        items[0].categoryName = capitalize(categoryName);
+
+                        return items;
+                    });
+                    callback(categories);
+                });
+            },
             name: 'a',
             displayKey: 'title',
             templates: {
                 suggestion: function(suggestion) {
+                    var html = [];
+                    if(suggestion.isCategoryHeader) {
+                        html.push('<div class="suggestion-category">' + suggestion.categoryName + '</div>');
+                    }
+
+                    var highlight = suggestion._highlightResult || {};
+                    var snippet = suggestion._snippetResult || {};
+                    var title = highlight.title.value;
+                    var text = '';
                     for(var i = 0 ; i < ATTRIBUTES.length ; i++) {
-                        if(suggestion._highlightResult[ATTRIBUTES[i]] !== undefined) {
-                            return '<span class="title"><strong>' + capitalize(suggestion._tags[1]) + '</strong>: ' + suggestion._highlightResult.title.value + '</span><span class="text">' + suggestion._highlightResult[ATTRIBUTES[i]].value + '</span>';
+                        if(highlight[ATTRIBUTES[i]] !== undefined) {
+                            text = highlight[ATTRIBUTES[i]].value;
+                            text = (snippet[ATTRIBUTES[i]] || {}).value || text;
+                            break;
                         }
                     }
+
+                    html.push('  <div class="suggestion-content">');
+                    html.push('    <div class="suggestion-title">' + title  + '</div>');
+                    html.push('    <div class="suggestion-text">' + text + '</div>');
+                    html.push('  </div>');
+                    return html.join(' ');
                 }
             }
         }
     ]).on('autocomplete:selected', function(event, suggestion) {
         window.location.href = suggestion.link;
     });
-})();
+})(jQuery);
