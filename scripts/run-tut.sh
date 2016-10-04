622434073d4b539c9f79b58e3ec868cca13aa98d
Jarrod Brockman <jarrodwb@jarrodwb.com>
2016-10-04 11:39:28 +0200

    try to get travis to give exit status 1 for run-tut script

--- scripts/run-tut.sh
+++ scripts/run-tut.sh
@@ -4,4 +4,3 @@ COURSIER_CLASSPATH="$(./coursier fetch -p com.chuusai:shapeless_2.11:2.3.1 org.s
 
 ./coursier launch -r "https://dl.bintray.com/tpolecat/maven/" org.tpolecat:tut-core_2.11:0.4.4 -- . tut-tmp '.*\.md$' -classpath "$COURSIER_CLASSPATH"
 
-rm -r tut-tmp
