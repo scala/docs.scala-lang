8135a18941364654a547d967928797a9dc7c8a04
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2017-12-05 16:00:38 +0100

    Update CI to check for dead links

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,6 +1,12 @@
-#!/bin/bash -
+#!/bin/bash
+
+set -eux
+
 . /usr/local/rvm/scripts/rvm
 bundle install
 ./scripts/run-tut.sh
 rm -r tut-tmp
 bundle exec jekyll build
+bundle exec htmlproofer ./_site/ --only-4xx --empty-alt-ignore --allow-hash-href
+
+exit 0
