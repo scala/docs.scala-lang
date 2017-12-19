c4f7924084f9f44bb3a2820cf69a08b4821c819a
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2017-12-19 10:27:19 +0100

    Ignore links to docs.scala-lang/blob/master

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -8,6 +8,8 @@ bundle install
 ./scripts/run-tut.sh
 rm -r tut-tmp
 bundle exec jekyll build
-bundle exec htmlproofer ./_site/ --only-4xx --empty-alt-ignore --allow-hash-href
+
+# Checking for docs.scala-lang/blob/master leads to a chicken and egg problem because of the edit links of new pages.
+bundle exec htmlproofer ./_site/ --only-4xx --empty-alt-ignore --allow-hash-href --url-ignore '/https://github.com/scala/docs.scala-lang/blob/master/.*/'
 
 exit 0
