202dbdea08110e9de7725fe2b66ed0423a98adf8
Heather Miller <heather.miller@epfl.ch>
2018-03-14 09:06:18 +0100

    Tell htmlproofer to ignore 401 no error status codes

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,6 +10,6 @@ rm -r tut-tmp
 bundle exec jekyll build
 
 # Checking for docs.scala-lang/blob/master leads to a chicken and egg problem because of the edit links of new pages.
-bundle exec htmlproofer ./_site/ --only-4xx --empty-alt-ignore --allow-hash-href --url-ignore '/https://github.com/scala/docs.scala-lang/blob/master/.*/'
+bundle exec htmlproofer ./_site/ --only-4xx --http-status-ignore "401" --empty-alt-ignore --allow-hash-href --url-ignore '/https://github.com/scala/docs.scala-lang/blob/master/.*/'
 
 exit 0
