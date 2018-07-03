99ae5a6bd710f66feb6903fcf965f003b6a294a6
Sakib Hadžiavdić <sake.etf@gmail.com>
2018-07-03 20:19:45 +0200

    Added contributors widget

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -522,3 +522,57 @@ $(document).ready(function(){
         return false; 
     }); 
 });
+
+//Contributors widget
+// see https://stackoverflow.com/a/19200303/4496364
+$(document).ready(function () {
+  let githubApiUrl = 'https://api.github.com/repos/scala/docs.scala-lang/commits';
+  // transform "/tour/basics.html" to "_ba/tour/basics.md";
+  let thisPageUrl = window.location.pathname;
+  thisPageUrl = thisPageUrl.substring(1, thisPageUrl.lastIndexOf('.'));
+  thisPageUrl = '_' + thisPageUrl + '.md';
+  // e.g. https://api.github.com/repos/scala/docs.scala-lang/commits?path=README
+  let url = githubApiUrl + '?path=' + thisPageUrl;
+  $.get(url, function (data, status) {
+    
+    let contributorsUnique = [];
+    let res = data.forEach(commit => {
+      // add if not already in array
+      let addedToList = contributorsUnique.find(c => {
+        let matches = c.authorName == commit.commit.committer.name;
+        if (!matches && commit.author) {
+          matches = c.authorName == commit.author.login;
+        }
+        return matches;
+      });
+
+      if (!addedToList) {
+        // first set fallback properties
+        let authorName = commit.commit.committer.name;
+        let authorLink = 'mailto:' + commit.commit.committer.email;
+        let authorImageLink = 'https://github.com/identicons/' + commit.commit.committer.name + '.png';
+        // if author present, fill these preferably
+        if (commit.author) {
+          authorName = commit.author.login;
+          authorLink = commit.author.html_url;
+          authorImageLink = commit.author.avatar_url;
+        }
+        contributorsUnique.push({
+          'authorName': authorName,
+          'authorLink': authorLink,
+          'authorImageLink': authorImageLink
+        });
+      }
+    });
+
+    let contributorsHtml = '';
+    contributorsUnique.forEach(contributor => {
+      let contributorHtml = '<div>';
+      contributorHtml += '<img src="' + contributor.authorImageLink + '">';
+      contributorHtml += '<a href="' + contributor.authorLink + '">' + contributor.authorName + '</a>';
+      contributorHtml += '</div>';
+      contributorsHtml += contributorHtml;
+    });
+    $('#contributors').html(contributorsHtml);
+  });
+});
