5ac9c209ec572f1be183ce8e6c55d2f2cd9d9f72
Sakib Hadžiavdić <sake92@users.noreply.github.com>
2018-07-07 12:52:20 +0200

    Added contributors widget (#1098)
    Fixes #44 
    
    Following [this answer](https://stackoverflow.com/a/19200303/4496364) I managed to get this via Github API:
    
    ![](https://upload.cc/i1/2018/07/04/n4zkhJ.png)
    
    I roughly followed Mozilla's [docs page](https://developer.mozilla.org/en-US/docs/Web/HTML) design and their contributors listing.
    Some users don't have the "author" field. Probably deleted their profile?  
    So, the fallback is `commit.commit.committer.name` which is, I suppose, always present because of Git...  
    
    Also, if the author isn't present, image icon is generated with https://github.com/identicons. Example here is Ghildiyal user.
    
    Currently, I added this to tour only. IDK did you guys intend to put these on each page?  
    If so, there could be some problems with getting page URL from Github. IDK if all of them follow same pattern as "tours"..

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -522,3 +522,84 @@ $(document).ready(function(){
         return false; 
     }); 
 });
+
+//Contributors widget
+// see https://stackoverflow.com/a/19200303/4496364
+$(document).ready(function () {
+  let githubApiUrl = 'https://api.github.com/repos/scala/docs.scala-lang/commits';
+  let identiconsUrl = 'https://github.com/identicons';
+  /* - we need to transform "/tour/basics.html" to "_ba/tour/basics.md"
+   * - some files aren't prefixed with underscore, see rootFiles
+   * - some files are placed in _overviews but rendered to its folder, see overviewsFolders
+   */
+  
+  let rootFiles = ['getting-started', 'learn', 'glossary'];
+  let overviewsFolders = ['FAQ', 'cheatsheets', 'collections', 'compiler-options',
+    'core', 'jdk-compatibility', 'macros', 'parallel-collections',
+    'plugins', 'quasiquotes', 'reflection',
+    'repl', 'scaladoc', 'tutorials'
+  ];
+
+  let thisPageUrl = window.location.pathname;
+  // chop off beginning slash and ending .html
+  thisPageUrl = thisPageUrl.substring(1, thisPageUrl.lastIndexOf('.'));
+  let isRootFile = rootFiles.some(rf => thisPageUrl.startsWith(rf));
+  let isInOverviewsFolder = overviewsFolders.some(of => thisPageUrl.startsWith(of));
+  if(isRootFile) {
+    thisPageUrl = thisPageUrl + '.md';
+  } else if(isInOverviewsFolder) {
+    thisPageUrl = '_overviews/'+ thisPageUrl + '.md';
+  } else {
+    thisPageUrl = '_' + thisPageUrl + '.md';
+  }
+  
+  let url = githubApiUrl + '?path=' + thisPageUrl;
+  $.get(url, function (data, status) {
+    if(!data || data.length < 1) {
+      $('.content-contributors').html(''); // clear content
+      return false; // break
+    }
+    let contributorsUnique = [];
+    data.forEach(commit => {
+      // add if not already in array
+      let addedToList = contributorsUnique.find(c => {
+        let matches = c.authorName == commit.commit.author.name;
+        if (!matches && commit.author) {
+          matches = c.authorName == commit.author.login;
+        }
+        return matches;
+      });
+
+      if (!addedToList) {
+        // first set fallback properties
+        let authorName = commit.commit.author.name;
+        let authorLink = '';
+        let authorImageLink = identiconsUrl + '/' + commit.commit.author.name + '.png';
+        // if author present, fill these preferably
+        if (commit.author) {
+          authorName = commit.author.login;
+          authorLink = commit.author.html_url;
+          authorImageLink = commit.author.avatar_url;
+        }
+        contributorsUnique.push({
+          'authorName': authorName,
+          'authorLink': authorLink,
+          'authorImageLink': authorImageLink
+        });
+      }
+    });
+
+    let contributorsHtml = '';
+    contributorsUnique.forEach(contributor => {
+      let contributorHtml = '<div>';
+      contributorHtml += '<img src="' + contributor.authorImageLink + '">';
+      if (contributor.authorLink)
+        contributorHtml += '<a href="' + contributor.authorLink + '">' + contributor.authorName + '</a>';
+      else
+        contributorHtml += '<a>' + contributor.authorName + '</a>';
+      contributorHtml += '</div>';
+      contributorsHtml += contributorHtml;
+    });
+    $('#contributors').html(contributorsHtml);
+  });
+});
