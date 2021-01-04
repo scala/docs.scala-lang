34d17d7737948b98155d7830547c65d42ce4ccc2
Jonathan Brachthäuser <jonathan@b-studios.de>
2021-01-04 11:17:55 +0100

    Ignore status code 400
    As per twitter's API description, 400 indicates an authentication problem.
    I am not trying to fix the underlying problem here, but merely ignore the
    error.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,6 +10,6 @@ rm -r /tmp/mdoc-out/
 bundle exec jekyll build
 
 # Checking for docs.scala-lang/blob/master leads to a chicken and egg problem because of the edit links of new pages.
-bundle exec htmlproofer ./_site/ --only-4xx --http-status-ignore "401,429" --empty-alt-ignore --allow-hash-href --url-ignore '/https://github.com/scala/docs.scala-lang/blob/master/.*/,/www.oracle.com/'
+bundle exec htmlproofer ./_site/ --only-4xx --http-status-ignore "400,401,429" --empty-alt-ignore --allow-hash-href --url-ignore '/https://github.com/scala/docs.scala-lang/blob/master/.*/,/www.oracle.com/'
 
 exit 0
