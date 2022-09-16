f658ce8685b9a0eef0ae6e75c5cd91f6cc75f566
Jamie Thompson <bishbashboshjt@gmail.com>
2022-09-16 17:53:09 +0200

    Update run-mdoc.sh
    try upgrade mdoc for scala 2.13

--- scripts/run-mdoc.sh
+++ scripts/run-mdoc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eux
 
-cs launch org.scalameta:mdoc_2.12:2.2.13 -- \
+cs launch org.scalameta:mdoc_2.13:2.3.3 -- \
   --in . \
   --out /tmp/mdoc-out/ \
-  --classpath $(cs fetch -p com.chuusai:shapeless_2.12:2.3.3) \
+  --classpath $(cs fetch -p com.chuusai:shapeless_2.13:2.3.10) \
   --scalac-options "-Xfatal-warnings -feature" \
   --no-link-hygiene \
   --include '**.md'
